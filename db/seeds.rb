482420cd071b7d606deb56311a59df103938110e
LB426 <LB426@tih.kuban.ru>
2012-02-02 17:42:35 +0000

    изменил seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,6 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 
-<<<<<<< HEAD
-ProductCategory.create([{ :category => 'Недвижимость' }, 
-                        { :category => 'Транспорт' },
-                        { :category => 'Предметы быта' },
-                        { :category => 'Услуги' },
-                        { :category => 'Разное' },])
-=======
 Location.create( :name => 'Тихорецк', :form => 'город' )
 
 n = ProductCategory.create( :category => 'Недвижимость' )
@@ -45,5 +38,3 @@ s = ProductSection.create(  :section => 'Аренда', :product_category_id => 
   ProductSubSection.create(  :subsection => 'Магазины', :product_category_id => n.id, :product_section_id => s.id )
   ProductSubSection.create(  :subsection => 'Склады', :product_category_id => n.id, :product_section_id => s.id )
   ProductSubSection.create(  :subsection => 'Прочее', :product_category_id => n.id, :product_section_id => s.id )                               
-                                                                                             
->>>>>>> 5ddd5deaf73c06becc1b5d20875c25f4cc6e2599
